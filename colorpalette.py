3620746f8ba6d572fd70db7e340c14cd5e7a1b73
140810230017 - Clarisya Adeline <144151876+ClarisyaA@users.noreply.github.com>
2025-05-29 00:01:23 +0700

    Update colorpalette.py

--- colorpalette.py
+++ colorpalette.py
@@ -40,7 +40,7 @@ def get_theme_css(is_dark_mode):
 
         /* File uploader styling */
         [data-testid="stFileUploader"] {
-            background-color: #ffffff !important;
+            background-color: #000000 !important;
             border: 1px solid #555 !important;
             border-radius: 10px;
         }
